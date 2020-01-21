f7f3486acab717e86e47734101f0a397372213af
Герман Труханов <germantruhanov@gmail.com>
2020-01-21 15:40:53 +0400

    work with contact view

--- AutoService.WEB/Controllers/HomeController.cs
+++ AutoService.WEB/Controllers/HomeController.cs
@@ -22,8 +22,6 @@ namespace AutoService.WEB.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
